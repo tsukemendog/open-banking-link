7095cf3041e13728de3cca2168d28457161f1de8
tsukemendog <tsukemendog@gmail.com>
2023-11-14 16:21:38 +0900

    프로젝트 생성 및 서버포트 변경

---
+++ src/main/java/com/tsukemendog/openbankinglink/OpenBankingLinkApplication.java
@@ -0,0 +1,13 @@
+package com.tsukemendog.openbankinglink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenBankingLinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenBankingLinkApplication.class, args);
+    }
+
+}
